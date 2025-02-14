2defae9bc5daeb2194da5c3b5551bf39cb9a571f
David Edey <david.edey@rdx.works>
2025-02-14 22:21:03 +0000

    fix: Fix compilation error test

--- tests/compilation_failures/expressions/code_blocks_are_not_reevaluated.rs
+++ tests/compilation_failures/expressions/code_blocks_are_not_reevaluated.rs
@@ -2,9 +2,12 @@ use preinterpret::*;
 
 fn main() {
     let _ = preinterpret!{
-        // We don't get a re-evaluation. Instead, we get a parse error, because we end up
-        // with let _ = [!error! "This was a re-evaluation"]; which is a parse error in
-        // normal rust land.
-        #(indirect = [!raw! [!error! "This was a re-evaluation"]]; #(indirect))
+        #(
+            let indirect = [!raw! [!error! "This was a re-evaluation"]];
+            // We don't get a re-evaluation. Instead, we get a parse error, because we end up
+            // with let _ = [!error! "This was a re-evaluation"]; which is a parse error in
+            // normal rust land.
+            #(indirect)
+        )
     };
 }
