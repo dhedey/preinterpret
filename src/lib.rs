d46ab336a6d7a2f2cc3edfa8d3d06a7d3df3fb11
David Edey <github@david-edey.com>
2025-10-06 15:37:24 +0100

    fix: Fix benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -586,7 +586,7 @@ mod benchmarking {
             let mut interpreter = Interpreter::new();
             block_content
                 .evaluate(&mut interpreter, Span::call_site().into())
-                .and_then(|x| x.into_new_output_stream(Grouping::Flattened))
+                .and_then(|x| x.into_stream())
                 .convert_to_final_result()
         });
         let interpreted_stream = interpreted_stream?;
