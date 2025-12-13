95258f84d16bca1ca252883304c7708d0cdaaaa0
David Edey <github@david-edey.com>
2025-12-13 15:08:23 +0000

    fix: Remove sandbox from build

--- src/lib.rs
+++ src/lib.rs
@@ -481,7 +481,6 @@ mod extensions;
 mod internal_prelude;
 mod interpretation;
 mod misc;
-mod sandbox;
 
 use internal_prelude::*;
 
--- src/sandbox/mod.rs
+++ src/sandbox/mod.rs
@@ -1,3 +1,2 @@
-#[cfg(false)] // Disable so it doesn't break MSRV build
 mod dyn_value;
 mod gat_value;
